a4e7f22ad1de6240f5f5b284de6c606dfe802b7c
ebbes <hutteric@live.de>
2016-10-09 11:22:19 +0200

    Bug fixes, re-switch on play/pause (useful on Alt+Tabbing out of Kodi, which results in 3D mode switched to 2Dto3D), pause playback during switching.

--- default.py
+++ default.py
@@ -35,6 +35,7 @@ class Service(xbmc.Player):
     def __init__(self):
         xbmc.Player.__init__(self)
         self.monitor = xbmc.Monitor()
+        self.abortRequested = False
         self.lgtv = LGTV(KodiKeyManager(), log=tools.simpleLog)
 
         self.isPlaying3D = None
@@ -48,6 +49,9 @@ class Service(xbmc.Player):
         self.lg_pairing_key = __addon__.getSetting('lg_pairing_key')
         self.enable_discovery = __addon__.getSetting('lg_enable_discovery')
         self.force_discovery = __addon__.getSetting('lg_force_discovery')
+        self.switch_on_pause = __addon__.getSetting('lg_switch_on_pause')
+        self.switch_on_resume = __addon__.getSetting('lg_switch_on_resume')
+        self.pause_while_switching = __addon__.getSetting('lg_pause_while_switching')
 
         host_was_empty = self.lg_host is None or self.force_discovery
         if host_was_empty and self.enable_discovery:
@@ -57,7 +61,7 @@ class Service(xbmc.Player):
             # no host found
             tools.notifyLog("No LG TV found on network and no TV is configured in settings")
             tools.notifyOSD(__addonname__, __LS__(30101), icon=__IconError__)
-            self.monitor.abortRequested = True
+            self.abortRequested = True
             return
 
         try:
@@ -85,12 +89,12 @@ class Service(xbmc.Player):
                     except Exception as e:
                         tools.notifyLog("Could not connect to TV at %s: %s" % (self.lg_host, str(e)), level=xbmc.LOGERROR)
                         tools.notifyOSD(__addonname__, __LS__(30100) % self.lg_host, icon=__IconError__)
-                        self.monitor.abortRequested = True
+                        self.abortRequested = True
             else:
                 # host found via recovery could not be connected to
                 tools.notifyLog("Could not connect to TV at %s: %s" % (self.lg_host, str(e)), level=xbmc.LOGERROR)
                 tools.notifyOSD(__addonname__, __LS__(30100) % self.lg_host, icon=__IconError__)
-                self.monitor.abortRequested = True
+                self.abortRequested = True
 
 
     def discover(self):
@@ -140,39 +144,85 @@ class Service(xbmc.Player):
         return False
 
     def onPlayBackStarted(self):
-        self.switch3D()
+        self.switch3D(self.pause_while_switching)
 
     def onPlayBackStopped(self):
-        self.switch3D()
+        self.switch3D(False)
 
     def onPlayBackEnded(self):
-        self.switch3D()
+        self.switch3D(False)
 
-    def switch3D(self):
+    def onPlayBackPaused(self):
+        # on Alt+Tab switching, TV might reset mode to 2Dto3D.
+        # Simply re-set current 3D mode when paused/resumed to
+        # have a quick way to re-set correct mode.
+        if self.switch_on_pause:
+            self.reswitch3D(False)
+
+    def onPlayBackResumed(self):
+        # on Alt+Tab switching, TV might reset mode to 2Dto3D.
+        # Simply re-set current 3D mode when paused/resumed to
+        # have a quick way to re-set correct mode.
+        if self.switch_on_resume:
+            self.reswitch3D(self.pause_while_switching)
+
+    def switch3D(self, auto_pause):
         if self.getStereoscopicMode():
-            tools.notifyLog('switching to 3D mode %s' % self.mode3D, level=xbmc.LOGDEBUG)
-            success, msg = self.lgtv.set_3D_Mode(self.mode3D)
-            if success:
-                return
-
-            # in case something _seriously_ failed during previous communication, try clean reconnect
-            if not self.lgtv.is_connected():
-                tools.notifyLog("Not connected, attempting reconnect")
-                success = self.lgtv.connect(self.lg_host, __addonname__)
-                if not success:
-                    tools.notifyLog("Reconnect failed")
-                    tools.notifyOSD(__addonname__, __LS__(30100) % self.lg_host, icon=__IconError__)
+            tools.notifyLog('switching to 3D mode %s' % self.mode3D)
+            if auto_pause:
+                # pause playback during switching
+                self.pause()
+            try:
+                success, msg = self.lgtv.set_3D_Mode(self.mode3D)
+                if success:
                     return
 
-                tools.notifyLog("Reconnected to TV at %s" % self.lg_host)
-                #tools.notifyOSD(__addonname__, __LS__(30102) % self.lg_host, icon=__IconConnected__)
-                self.lgtv.toast(__LS__(30104), icon_file=__IconKodi__)
+                # in case something _seriously_ failed during previous communication, try clean reconnect
+                if not self.lgtv.is_connected():
+                    tools.notifyLog("Not connected, attempting reconnect")
+                    success = self.lgtv.connect(self.lg_host, __addonname__)
+                    if not success:
+                        tools.notifyLog("Reconnect failed")
+                        tools.notifyOSD(__addonname__, __LS__(30100) % self.lg_host, icon=__IconError__)
+                        return
 
-            success, msg = self.lgtv.set_3D_Mode(self.mode3D)
-            if not success:
-                tools.notifyLog(msg)
-                if not self.lgtv.toast(msg, icon_file=__IconKodi__):
-                    tools.notifyOSD(__addonname__, msg, icon=__IconError__)
+                    tools.notifyLog("Reconnected to TV at %s" % self.lg_host)
+                    #tools.notifyOSD(__addonname__, __LS__(30102) % self.lg_host, icon=__IconConnected__)
+                    self.lgtv.toast(__LS__(30104), icon_file=__IconKodi__)
+
+                success, msg = self.lgtv.set_3D_Mode(self.mode3D)
+                if not success:
+                    tools.notifyLog(msg)
+                    if not self.lgtv.toast(msg, icon_file=__IconKodi__):
+                        tools.notifyOSD(__addonname__, msg, icon=__IconError__)
+            finally:
+                if auto_pause:
+                    # resume playback after switching
+                    self.pause()
+
+    def reswitch3D(self, auto_pause):
+        mode = self.lgtv.get_3D_Mode()
+
+        if mode == Display3dMode.ERROR:
+            tools.notifyLog("Could not get current 3D mode")
+            if not self.lgtv.toast("Could not get current 3D mode", icon_file=__IconKodi__):
+                tools.notifyOSD(__addonname__, "Could not get current 3D mode", icon=__IconError__)
+
+        if mode == self.mode3D:
+            return
+
+        if auto_pause:
+            # pause playback until 3D mode is switched
+            self.pause()
+        success, msg = self.lgtv.set_3D_Mode(self.mode3D)
+        if not success:
+            tools.notifyLog(msg)
+            if not self.lgtv.toast(msg, icon_file=__IconKodi__):
+                tools.notifyOSD(__addonname__, msg, icon=__IconError__)
+        if auto_pause:
+            # resume
+            self.pause()
+
 
     def keepConnectionAlive(self):
         self.lgtv.send_pong()
@@ -180,7 +230,7 @@ class Service(xbmc.Player):
 if __name__ == '__main__':
     service = Service()
 
-    while not service.monitor.abortRequested():
+    while not service.monitor.abortRequested() and not service.abortRequested:
         if service.monitor.waitForAbort(PONG_INTERVAL):
             break
         # send pong message (does not result in server response)
